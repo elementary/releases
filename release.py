e96eac14b2ab794b7265f8f7a46dbd95f286980a
David Hewitt <davidmhewitt@gmail.com>
2020-04-02 10:40:13 +0100

    Get number of commits since last release

--- release.py
+++ release.py
@@ -1,41 +1,58 @@
 import datetime
 import json
 import os
+import sys
 from github import Github
+from github.GithubException import UnknownObjectException as GithubUnknownObjectException
 
 g = Github(os.environ['GITHUB_TOKEN'])
 
+class NoReleasesFound(Exception):
+  pass
+
 class Repo:
-  def __init__(self, repo, release, last_release_timestamp):
+  def __init__(self, repo):
     self.repo = repo
-    self.release = release
-    self.last_release_timestamp = last_release_timestamp
+
+    try:
+      self.release = self.repo.get_latest_release()
+    except GithubUnknownObjectException:
+      raise NoReleasesFound
+
+    self.last_release_timestamp = self.release.created_at.timestamp()
+    self.commits = self.repo.get_commits(since=self.release.created_at,sha='master')
 
 org = g.get_organization('elementary')
 
 repos = []
 for repo in org.get_repos():
-  release = None
+  print ('Processing {}...'.format(repo.name))
   try:
-    release = repo.get_latest_release()
-  except:
+    repos.append(Repo(repo))
+  # This catches repos that don't have releases and ignores them
+  except NoReleasesFound:
     pass
-    
-  repos.append(Repo(repo, release, release.created_at.timestamp() if release else 0))
 
-repos = filter(lambda repo: repo.last_release_timestamp != 0, repos)  
 repos = sorted(repos, key=lambda repo: repo.last_release_timestamp, reverse=True)
 
 json_out = []
 for repo in repos:
   release_date = datetime.datetime.fromtimestamp(repo.last_release_timestamp).strftime("%Y-%m-%d")
+  commits_since = 0
+  try:
+    # Remove 1 commit, since the release commit itself seems to get included
+    commits_since = repo.commits.totalCount - 1
+    if commits_since < 0:
+      commits_since = 0
+  except GithubUnknownObjectException:
+    pass
+
   json_out.append ({
     "repo": repo.repo.name,
     "version": repo.release.tag_name,
-    "release_date": release_date
+    "release_date": release_date,
+    "commits_since": commits_since
   })
-
-  print('{} ({}) released {}'.format (repo.repo.name, repo.release.tag_name, release_date))
   
 with open('_data/releases.json', 'w') as outfile:
-    json.dump(json_out, outfile)
+    json.dump(json_out, outfile, indent=2)
